5f1b5439065cd206c323c30f6c9fbc548826bfd8
Dekup <nama47150@gmail.com>
2026-01-09 15:50:14 +0800

    Добавлены комментарии
    Удалена бесполезная строчка, улучшена структура для более читаемого кода

--- maps.py
+++ maps.py
@@ -1,16 +1,16 @@
 import random as ran, cursor as cur, os
-
+# высота и ширина карты
 height = 46
 width = 156
-final = ""
 
+# создание разных биомов
 water = {"char": "~", "color": ((0, 1), (50, 100), (200, 255))}
 mountains = {"char": "▲", "color": ((130, 135), (133, 135), (130, 135))}
 snow = {"char": "*", "color": (255, 255, 255)}
 beach = {"char": ".", "color": ((200, 255), (200, 255), (0, 50))}
 grass = {"char": "_", "color": ((50, 60), (200, 255), (100, 150))}
 
-# определение территории и создание функции генерации
+# функция генерации
 def generate(teritory):
     colors = teritory["color"]
     if not teritory == snow:
@@ -22,11 +22,13 @@ def generate(teritory):
         g = colors[1]
         b = colors[2]
     print(f"\033[38;2;{r};{g};{b}m{teritory["char"]}\033[0m", end="", flush=True)
+# основная отрисовка
 while True:
     with cur.HiddenCursor():
         for y in range(height):
             for x in range(width):
                 val = ran.randint(0, 100)
+                # определение территории
                 if val < 20: ter = water
                 elif 20 <= val < 30: ter = beach
                 elif 30 <= val < 65: ter = grass
@@ -36,3 +38,4 @@ while True:
             print("")
     input("\nНажмите Enter чтобы сгенерировать заново: ")
     os.system("cls")
+
